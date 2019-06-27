f8e8b93ddb9a900d81f72b3a927f56bc77fe6762
Marco Frisina <marco@frisina.xyz>
2019-06-27 16:22:14 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,31 +1,90 @@
 # My Code here....
 def map_to_negativize(source_array)
-  source_array.map {|n| n * -1}
+  i = 0;
+  newArray = [];
+  while source_array[i] do
+    newArray.push(source_array[i] * -1)
+    i += 1
+  end
+  return newArray
+  #source_array.map {|n| n * -1}
 end
 
 def map_to_no_change(source_array)
-  source_array.map {|n| n}
+  i = 0;
+  newArray = [];
+  while source_array[i] do
+    newArray.push(source_array[i])
+    i += 1
+  end
+  return newArray
 end
 
+  #source_array.map {|n| n}
+
+
 def map_to_double(source_array)
-  source_array.map {|n| n * 2}
+  #source_array.map {|n| n * 2}
+  i = 0;
+  newArray = [];
+  while source_array[i] do
+    newArray.push(source_array[i] * 2)
+    i += 1
+  end
+  return newArray
 end
 
 def map_to_square(source_array)
-  source_array.map {|n| n * n}
+  i = 0;
+  newArray = []
+  while source_array[i] do
+    newArray.push(source_array[i] * source_array[i])
+    i += 1
+  end
+  return newArray
 end
+  #source_array.map {|n| n * n}
 
-def reduce_to_total(source_array, starting_point)
+def reduce_to_total(source_array, starting_point=0)
+  returnValue = 0
+
   if starting_point
-    return source_array.reduce(starting_point) { |sum, n| sum + n }
+      returnValue = source_array.inject(starting_point, :+)
   else
-    return source_array.sum
+      returnValue = source_array.inject(0, :+)
+  end
+    return returnValue
 end
+  #  return source_array.reduce(starting_point) { |sum, n| sum + n }
+  #  else
+  #  return source_array.sum
 
-def reduce_to_all_true(source_array)
-  source_array.all?
-end
+  def reduce_to_all_true(source_array)
+    i = 0;
+    returnValue = true
+
+    while i < source_array.length do
+      if (!!source_array[i] == false)
+        returnValue = false
+      end
+        i += 1
+    end
+    return returnValue
+  end
+  #source_array.all?
+
 
 def reduce_to_any_true(source_array)
-  source_array.any?
+  i = 0;
+  returnValue = false
+
+  while i < source_array.length do
+    if (!!source_array[i] == true)
+      returnValue = true
+    end
+      i += 1
+  end
+  return returnValue
 end
+
+  #source_array.any?
